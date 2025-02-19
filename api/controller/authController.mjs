2dbe2bb2c91aa9232654eef059491c6b9479104c
ghermaico135 <ghermaico135@gmail.com>
2025-02-19 08:13:46 -0800

    fixing register page

--- api/controller/authController.mjs
+++ api/controller/authController.mjs
@@ -1,29 +1,28 @@
 import db from "../db.mjs"
-import bcrypt  from "bcrypt"
+import bcrypt from "bcrypt";
 
-export function register(req,res){
-
-    const q = "SELECT * FROM users WHERE email= ? OR username = ?"
-    db.query(q, [req.body.email,req.body.username], (err, data) =>{
-        if(err) return res.json(err)
-        if(data.length) return res.status(409).json("User already exists")
-        
-        // Hash the password and create user
-
-        const salt = bcrypt.genSaltSync(10)
-        const hash = bcrypt.hashSync(req.body.password,salt)
-
-        const q = "INSERT INTO users('username','email','password') VALUES ( ?)"
-        const values = [req.body.username,req.body.email,hash]
-
-        db.query(q,[values],(err,data) =>{
-            if(err) return res.json(err)
-            return res.status(200).json("User has been created")
-        })
-    })
-
+export const register = (req,res) =>{
+        const q = "SELECT * FROM users WHERE email= ? OR username = ?"
+        db.query(q, [req.body.email,req.body.username], (err, data) =>{
+            if(err) return res.status(500).json(err)
+            if(data.length) return res.status(409).json("User already exists")
+            
+            // Hash the password and create user
     
-    }
+            const salt = bcrypt.genSaltSync(10)
+            const hash = bcrypt.hashSync(req.body.password,salt)
+    
+            const insertq = "INSERT INTO users('username','email','password') VALUES (?,?,?)"
+            const values = [req.body.username,req.body.email,hash]
+    
+            db.query(insertq,values,(err,data) =>{
+                if(err) return res.status(500).json(err)
+                return res.status(200).json("User has been created")
+            })
+        })
+    
+        }
+
 export function login(req,res){
     
     }
--- api/routes/auth.mjs
+++ api/routes/auth.mjs
@@ -1,8 +1,10 @@
 import express from "express"
 import { register,login,logout } from "../controller/authController.mjs"
+
 const router = express.Router()
 
 router.post("/register" ,register)
+
 router.post("/login" ,login)
 router.post("/logout" ,logout)
 
